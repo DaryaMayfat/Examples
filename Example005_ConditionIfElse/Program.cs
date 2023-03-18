97dbd103701b1e89d103ba86940b1255ab2e89fb
Darya Maifat <darya885@gmail.com>
2023-03-19 02:01:53 +0600

    IfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write ("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username == "Дарья");
+{
+    Console.WriteLine("Ура, это же Дарья");
+}
+    else
+{
+   Console.write ("Привет, ");
+    Console.WriteLine(username); 
+}
