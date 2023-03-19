26a777b2e6716bd91ef6d56e8d4cfe0dccb53360
Darya Maifat <darya885@gmail.com>
2023-03-20 00:16:06 +0600

    Исправили ошибку

--- Example005_ConditionIfElse/Program.cs
+++ Example005_ConditionIfElse/Program.cs
@@ -1,12 +1,12 @@
-﻿Console.Write("Введите имя пользователя");
+﻿Console.Write("Введите имя пользователя ");
 string username = Console.ReadLine();
 
-if(username.ToLower() == "Дарья");
+if(username.ToLower() == "Дарья")
 {
     Console.WriteLine("Ура, это же Дарья");
 }
 else
 {
-     Console.Write("Привет, ");
+    Console.Write("Привет, ");
     Console.WriteLine(username);
 }
